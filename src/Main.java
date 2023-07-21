f1c835da02ef4d02e8bc026116d8fe8b31e927f3
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-21 11:07:38 +0200

    Klasse Rechteck und den Wunsch, eine Methode zur Ausgabe zu benutzen,hinzugefügt!

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Kreis kreis = new Kreis(2.5);
         Quadrat quadrat = new Quadrat(3);
+        Rechteck rechteck = new Rechteck(1.9, 2.1);
 
+        ausgabeBerechnungen(kreis);
+//        ausgabeBerechnungen(quadrat);
+//        ausgabeBerechnungen(rechteck);
+    }
+
+    private static void ausgabeBerechnungen(Kreis kreis) {
         System.out.println("Fläche vom Kreis: " + kreis.berechneFläche());
         System.out.println("Umfang vom Kreis: " + kreis.berechneUmfang());
-        System.out.println("Fläche vom Quadrat: " + quadrat.berechneFläche());
-        System.out.println("Umfang vom Quadrat: " + quadrat.berechneUmfang());
     }
 }
---
+++ src/Rechteck.java
@@ -0,0 +1,19 @@
+public class Rechteck extends Grundform {
+    private double länge;
+    private double breite;
+
+    public Rechteck(double länge, double breite) {
+        this.länge = länge;
+        this.breite = breite;
+    }
+
+    @Override
+    public double berechneFläche() {
+        return länge * breite;
+    }
+
+    @Override
+    public double berechneUmfang() {
+        return 2 * (länge + breite);
+    }
+}
